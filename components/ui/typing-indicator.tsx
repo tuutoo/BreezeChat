6aefdd79b5814244d4020f40044fb334e9df33ee
tuutoo <tuutoo@gmail.com>
2025-05-27 21:37:08 +0800

    feat: make dot bigger

--- components/ui/typing-indicator.tsx
+++ components/ui/typing-indicator.tsx
@@ -5,9 +5,9 @@ export function TypingIndicator() {
     <div className="justify-left flex space-x-1">
       <div className="rounded-lg bg-muted p-3">
         <div className="flex -space-x-2.5">
-          <Dot className="h-5 w-5 motion animate-pulse" />
-          <Dot className="h-5 w-5 motion animate-pulse delay-[0.50s]" />
-          <Dot className="h-5 w-5 motion animate-pulse delay-[1.0s]" />
+          <Dot className="h-6 w-6 motion animate-pulse" />
+          <Dot className="h-6 w-6 motion animate-pulse delay-[0.50s]" />
+          <Dot className="h-6 w-6 motion animate-pulse delay-[1.0s]" />
         </div>
       </div>
     </div>
