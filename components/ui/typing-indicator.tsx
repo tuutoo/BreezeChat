36a9e85683e3e335c2820a42c9290be6fd2063c5
tuutoo <tuutoo@gmail.com>
2025-05-27 13:34:12 +0800

    feat: add animation

--- components/ui/typing-indicator.tsx
+++ components/ui/typing-indicator.tsx
@@ -5,9 +5,9 @@ export function TypingIndicator() {
     <div className="justify-left flex space-x-1">
       <div className="rounded-lg bg-muted p-3">
         <div className="flex -space-x-2.5">
-          <Dot className="h-5 w-5 animate-typing-dot-bounce" />
-          <Dot className="h-5 w-5 animate-typing-dot-bounce [animation-delay:90ms]" />
-          <Dot className="h-5 w-5 animate-typing-dot-bounce [animation-delay:180ms]" />
+          <Dot className="h-5 w-5 motion animate-pulse" />
+          <Dot className="h-5 w-5 motion animate-pulse delay-[0.50s]" />
+          <Dot className="h-5 w-5 motion animate-pulse delay-[1.0s]" />
         </div>
       </div>
     </div>
